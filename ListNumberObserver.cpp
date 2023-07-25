081088b339bc7379708a8f691af66f5be27abcfe
daiji <jialu.dai@stud.unifi.it>
2023-07-25 21:49:38 +0200

    add ListNumberObserver
    Signed-off-by: daiji <jialu.dai@stud.unifi.it>

---
+++ ListNumberObserver.cpp
@@ -0,0 +1,21 @@
+//
+// Created by daiji on 25/07/2023.
+//
+
+#include <iostream>
+#include "ListNumberObserver.h"
+
+ListNumberObserver::ListNumberObserver(ShoppingListManager *s) : subject(s), listnumber(0){
+    subject->subscribe(this);
+}
+
+void ListNumberObserver::update() {
+    listnumber=subject->getlistnumber();
+}
+
+void ListNumberObserver::printalllists() {
+    update();
+    std::cout<<"printing my all lists"<<std::endl;
+    subject->printlists();
+    std::cout<<"ci sono "<<listnumber<<" liste."<<std::endl;
+}
